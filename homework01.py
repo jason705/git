f536dbf4ad9f1da5d30303d1229570a96233ce67
jason705 <s850705a@gmail.com>
2016-03-03 16:12:04 +0800

    homework

---
+++ homework01.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+str1='資訊管理學系107級';
+str2='A1033311';
+str3='商愷宸';
+str4='shung kai chung';
+print unicode(str1,'utf-8');
+print unicode(str2,'utf-8');
+print unicode(str3,'utf-8');
+print unicode(str4,'utf-8');
